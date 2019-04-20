bb4ef9f9dd6010c35a36b0e144ecd834673b0ed5
osmak <osmak@protonmail.com>
2019-04-20 22:57:40 +0200

    updated settings

---
+++ newsapp/src/app/service/settings-response.ts
@@ -0,0 +1,4 @@
+export interface SettingsResponse {
+    lang: string,
+    country: string
+}
--- newsapp/src/app/settings/settings.page.ts
+++ newsapp/src/app/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../service/settings.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  lang;
+  country;
+  favLang;
+  id = '5caf599e46f70034cf7ba93d' //todo change id to actual id you want to update
 
-  ngOnInit() {
+  constructor(public service: SettingsService, public toastController: ToastController) {
+    this.lang = 'en'
+    this.country = 'us'
   }
+
+  async ngOnInit() {
+  
+    await this.service.getLangFav(this.id).subscribe(async (data)=>{
+      this.favLang = await data
+      console.log(data)
+    })  
+
+  }
+
+
+  onLangChange() {
+    console.log("Idioma seleccionado: " + this.lang)
+
+    this.service.updateLang(this.id, this.lang).subscribe(async (data) => {
+      console.log(data)
+      const toast = await this.toastController.create({
+        message: 'Updated language!',
+        duration: 2000
+      });
+      toast.present();
+    },
+      (error) => {
+        console.log(error)
+      })
+
+
+  }
+
+  onCountryChange() {
+    console.log("País seleccionado: " + this.country)
+
+    this.service.updateCountry(this.id, this.country).subscribe(async (data) => {
+      console.log(data)
+      const toast = await this.toastController.create({
+        message: 'Updated country!',
+        duration: 2000
+      });
+      toast.present();
+    },
+      (error) => {
+        console.log(error)
+      })
+  }
+
 
 }
